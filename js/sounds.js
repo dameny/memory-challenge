61378317e034147e5d0eb23096d496d9865ee029
dameny <dameny87@gmail.com>
2021-11-27 17:12:35 -0800

    Update paths for sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,8 @@
 const buttonSounds = {};
-const gameOverSound = new Audio('..\\sounds\\wrong.mp3');
+const gameOverSound = new Audio('sounds/wrong.mp3');
 
 for (const color of buttonColours) {
-  buttonSounds[color] = new Audio(`..\\sounds\\${color}.mp3`);
+  buttonSounds[color] = new Audio(`sounds/${color}.mp3`);
 }
 
 function playGameOverSound() {
